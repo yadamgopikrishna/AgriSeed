01c80f1d0cad6b66eeb5c770cbd4a386042dfc24
YADAM GOPIKRISHNA <gopikrishnayadam53@gmail.com>
2026-07-24 01:25:43 +0530

    added registarion page and updated styles

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ print("current folder:", os.getcwd())
 app= Flask(__name__)
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
+@app.route("/register")
+def register():
+    return render_template("register.html")
 if __name__=="__main__":
     app.run(debug=True)
